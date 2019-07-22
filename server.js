91eceda658a85d2efbbf98d7ed6d671cfdeb12f5
jim9112 <jim9112@gmail.com>
2019-07-22 17:58:02 -0400

    error handling and route aditions

--- server.js
+++ server.js
@@ -6,14 +6,15 @@ const app = express();
 // connect database
 connectDB();
 
-
+// init middleware 
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API....'}));
 
 // define routes
-app.use('/api/users', require('./routs/users'));
-app.use('/api/auth', require('./routs/auth'));
-app.use('/api/contacts', require('./routs/contacts'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 
 const PORT = process.env.PORT  || 5000;
