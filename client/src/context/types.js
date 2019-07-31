4e421a2a309f8e95d39d335d8222691b7038c97a
jim9112 <jim9112@gmail.com>
2019-07-31 13:31:13 -0400

    Add contact state

---
+++ client/src/context/types.js
@@ -0,0 +1,9 @@
+export const ADD_CONTACT = 'ADD-CONTACT';
+export const DELETE_CONTACT = 'DELETE-CONTACT';
+export const SET_CURRENT = 'SET_CURRENT';
+export const CLEAR_CURRENT = 'CLEAR_CURRENT';
+export const UPDATE_CONTACT = 'UPDATE-CONTACT';
+export const FILTER_CONTACTS = 'FILTER-CONTACTS';
+export const CLEAR_FILTER = 'CLEAR_FILTER';
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
