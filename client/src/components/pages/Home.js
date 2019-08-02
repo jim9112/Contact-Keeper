a36501d0a26982a917f38a42aa6c83e098d97459
jim9112 <jim9112@gmail.com>
2019-08-02 14:16:18 -0400

    Add Contact Functionality

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Contacts from '../Contacts/Contacts'
+import Contacts from '../Contacts/Contacts';
+import ContactForm from '../Contacts/ContactForm';
 
 const Home = () => {
     return (
         <div className='grid-2'>
             <div>
-            Hello
+                <ContactForm />
             </div>
             <div>
                 <Contacts />
