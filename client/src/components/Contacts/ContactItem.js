e780b753ec7d953d2f6af2957ea0719f749acb0d
jim9112 <jim9112@gmail.com>
2019-08-02 11:47:44 -0400

    add buttons to contact cards

--- client/src/components/Contacts/ContactItem.js
+++ client/src/components/Contacts/ContactItem.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ContactItem = ({ contact }) => {
     const { id, name, email, phone, type } = contact;
@@ -19,8 +20,16 @@ const ContactItem = ({ contact }) => {
                     <i className="fas fa-phone"></i> {phone}
                 </li>)}
             </ul>
+            <p>
+                <button className="btn btn-dark small">Edit</button>
+                <button className="btn btn-danger small">Delete</button>
+            </p>
         </div>
     )
+}
+
+ContactItem.propTypes = {
+    contact: PropTypes.object.isRequired
 }
 
 export default ContactItem
