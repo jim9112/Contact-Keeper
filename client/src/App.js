d7cb7216618157fc387cc5d1efd6842d80b30a8c
jim9112 <jim9112@gmail.com>
2019-08-19 17:04:40 -0400

    login and register paths

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+/* eslint-disable react/jsx-filename-extension */
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
@@ -18,14 +21,16 @@ const App = () => {
             <Navbar />
             <div className="container">
               <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
               </Switch>
             </div>
           </Fragment>
         </Router>
-        </ContactState>
-      </AuthState>
+      </ContactState>
+    </AuthState>
   );
 };
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,24 +1,30 @@
+/* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({ title, icon }) => {
-    return (
-        <div className='navbar bg-primary'>
-           <h1>
-            <i className={icon}>{title}</i>   
-            </h1>
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/about'>About</Link>
-                </li>
-            </ul>    
-        </div> 
-        
-    )
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <i className={icon}>{title}</i>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 Navbar.propTypes = {
