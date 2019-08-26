0b215c83eb9f2e35a75bc0bdd31b656d2214172c
jim9112 <jim9112@gmail.com>
2019-08-26 18:30:04 -0400

    working login form on front end

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -64,7 +64,29 @@ const AuthState = (props) => {
     }
   }
   // login
+  const login = async (formData) => {
+    const config = {
+      headers: {
+        'content-type': 'application/json'
+      }
+    }
 
+    try {
+      const res = await axios.post('./api/auth', formData, config);
+
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: res.data
+      });
+
+      loadUser();
+    } catch (err) {
+      dispatch({
+        type: LOGIN_FAIL,
+        payload: err.response.data.msg
+      });
+    }
+  }
   // logout
 
   // clear errors
@@ -81,6 +103,7 @@ const AuthState = (props) => {
         register,
         clearErrors,
         loadUser,
+        login
       }}>
       { props.children }
     </AuthContext.Provider>
